0890b6e00e782cbc886aa41df97540e2bd5bf5d4
Martin Procházka <prochazka@cngroup.dk>
2023-05-21 10:27:31 +0200

    ValidationHelperTests

---
+++ CardValidator/Tests/ValidationHelperTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CardValidator.Helpers;
+using Xunit;
+// ReSharper disable InlineOutVariableDeclaration
+
+namespace CardValidator.Tests;
+
+public class ValidationHelperTests
+{
+    [Fact]
+    public void IsValidFormat_ValidFormat_ReturnsTrueAndCleanCardNumber()
+    {
+        // Arrange
+        var input = "1234 5678 9012 3456".AsSpan();
+        string? cleanCardNumber;
+
+        // Act
+        var result = input.IsValidFormat(out cleanCardNumber);
+
+        // Assert
+        Assert.True(result);
+        Assert.Equal("1234567890123456", cleanCardNumber);
+    }
+
+    [Fact]
+    public void IsValidFormat_InvalidFormat_ReturnsFalseAndNullCleanCardNumber()
+    {
+        // Arrange
+        var input = "12ab34 5678 9012 3456".AsSpan();
+        string? cleanCardNumber;
+
+        // Act
+        var result = input.IsValidFormat(out cleanCardNumber);
+
+        // Assert
+        Assert.False(result);
+        Assert.Null(cleanCardNumber);
+    }
+}
